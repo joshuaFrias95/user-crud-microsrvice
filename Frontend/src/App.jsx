590dae9141a8446cc1646af3c1081cd5ed1011a6
Jandres373 <82553481+Jandres373@users.noreply.github.com>
2024-04-08 18:54:24 -0500

    general fixes

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,17 +1,15 @@
-import { useState, useEffect } from "react";
 import "./styles/App.css";
 import { apiRequest } from "./utils/fetch";
 
 function App() {
-  const [data, setData] = useState(null);
 
-  const handleGetUsers = () => {
-    apiRequest.get('/users')
+
+  const handleGetUsers = async () => {
+    console.log(await apiRequest.get('/users'))
   };
 
   return (
     <>
-      <div></div>
       <div>adios</div>
       <button onClick={handleGetUsers}>Traer Datos de usuario</button>
     </>
