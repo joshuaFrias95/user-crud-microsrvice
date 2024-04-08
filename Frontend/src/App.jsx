c44d0d93bfe979bddede58e2faeeeaa7173bbf54
Joshua F <metatron1495@gmail.com>
2024-04-08 16:01:14 -0600

    Add: Concluyo arquitectura de microservicios BackEnd/FrontEnd

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,27 @@
+import { useState, useEffect } from 'react'
+import './styles/App.css'
+import { Fetch } from "./utils/fetch"
+
+function App() {
+
+ 
+ /* const [users, setUsers] = useState(null)
+  useEffect(() => {
+    async function getUser() {
+        const apiGet = new Fetch(8080)};
+        console.log(await apiGet.get("/users"));
+      }
+      getUser()
+  }, []); */
+
+
+  return (
+    <>
+    <div></div>
+    <div>adios</div>
+    <button onClick={''}>Traer Datos de usuario</button>
+    </>
+  )
+}
+
+export default App
---
+++ Frontend/src/utils/fetch.js
@@ -0,0 +1,22 @@
+export class Fetch {
+   /* BASE_URL = "http://localhost:";
+    PORT;
+
+    constructor(port) {
+        this.PORT = port;
+    }
+
+    async get(path) {
+        const URL = ${this.BASE_URL}${this.PORT}/api/v1${path};
+        const res = await fetch(URL, {
+        method: "GET",
+        headers: {
+          'Content-Type': 'application/json',
+        },
+       });
+
+       const data = await res.json();
+       return data;
+    }*/
+}
+
